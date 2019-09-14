408e9301e3401e5b4ac95c9d874ffd6a0f817fb0
James Brinkerhoff <James@devengage.io>
2019-09-13 17:56:43 -0700

    progress

---
+++ src/trigger/analytics-model.trigger.ts
@@ -0,0 +1,56 @@
+import { EngageFirestore, EngageAnalytics, IEngageTriggerData } from '../functions';
+import { IEngageAnalyticModel } from '../interfaces/analytics.interface';
+
+export class EngageAnalyticsTrigger {
+    engine: EngageAnalytics;
+
+    constructor(
+        public path, 
+        public triggers?: IEngageAnalyticModel[],
+    ) {
+        this.engine = new EngageAnalytics(`$${path}`);
+        this.init();
+    }
+
+    private async init() {
+        if (!this.triggers) {
+            this.triggers = await this.engine.getTiggers(this.path);
+        }
+    }
+
+    updateDestinations(data: IEngageTriggerData) {
+        const details = this.getPathDetails(this.path)
+        const path = `${details.collection}/${data.id}`;
+        const engine = new EngageAnalytics(path);
+        engine.updateDestinations(this.triggers, data);
+    }
+
+    async onWrite(data: IEngageTriggerData) {
+        this.updateDestinations(data.data);
+    }
+
+    async onCreate(data: IEngageTriggerData) {
+        this.updateDestinations(data.data);
+    }
+
+    async onUpdate(data: IEngageTriggerData) {
+        this.updateDestinations(data.data);
+    }
+
+    async onDelete(data: IEngageTriggerData) {
+        this.updateDestinations(data.data);
+    }
+
+    getPathDetails(path: string) {
+        const [collection, id, subCollection, subId] = path.split('/');
+        return {
+            collection,
+            id,
+            subCollection,
+            subId,
+            name: `${collection}${(subCollection || '').toUpperCase()}`,
+            trigger: path,
+        };
+    }
+
+}
